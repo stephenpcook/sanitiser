124c34a02c7cfe1b8e617f29360402e3d8c85579
Stephen P. Cook <spc67@bath.ac.uk>
2026-04-22 10:46:15 +0100

    Add count and check arguments

--- src/sanitiser/__init__.py
+++ src/sanitiser/__init__.py
@@ -12,19 +12,31 @@ __all__ = ["__version__", "main", "sanitise_text"]
 
 
 def sanitise_text(
-    infile: TextIO, word_map: dict[str, str], *, remove_hyperlinks: bool = True
-) -> None:
-    """Remove sensitive words and links from a file."""
+    infile: TextIO,
+    word_map: dict[str, str],
+    *,
+    remove_hyperlinks: bool = True,
+    no_output: bool = False,
+) -> int:
+    """Remove sensitive words and links from text and return change count.
+
+    Prints to output to stdout and returns the number of lines changed."""
     patterns = []
     for k, v in word_map.items():
         patterns.append((re.compile(rf"{k}", re.IGNORECASE), v))
     with infile as f:
+        lines_changed = 0
         for line in f:
+            line_before_subs = line
             if remove_hyperlinks:
                 line = strip_links(line)
             for pat, repl in patterns:
                 line = re.sub(pat, repl, line)
-            print(line, end="")
+            if line_before_subs != line:
+                lines_changed += 1
+            if not no_output:
+                print(line, end="")
+    return lines_changed
 
 
 def main() -> None:
@@ -35,14 +47,22 @@ def main() -> None:
     parser.add_argument(
         "--hyperlinks", action=argparse.BooleanOptionalAction, default=False
     )
+    parser.add_argument(
+        "--count",
+        action="store_true",
+        help="return only the number of lines which would change",
+    )
+    parser.add_argument(
+        "--check",
+        action="store_true",
+        help="as for --count, and exit non-zero if changes required",
+    )
     parser.add_argument("--word-map-directory", type=Path, action="append")
-
     parser.add_argument(
         "--show-map-paths",
         action="store_true",
         help="show all word map paths and files and exit",
     )
-
     parser.add_argument(
         "infile", nargs="?", type=argparse.FileType("r"), default=sys.stdin
     )
@@ -59,4 +79,13 @@ def main() -> None:
         return
 
     word_map = dict(get_word_maps(word_map_files))
-    sanitise_text(args.infile, word_map, remove_hyperlinks=not args.hyperlinks)
+    n_changes = sanitise_text(
+        args.infile,
+        word_map,
+        remove_hyperlinks=not args.hyperlinks,
+        no_output=args.count or args.check,
+    )
+    if args.count or args.check:
+        print(n_changes)
+    if args.check and n_changes > 0:
+        exit(1)
